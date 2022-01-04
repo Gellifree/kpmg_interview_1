241cc98746c55ef105ecace995fadce440878e11
Gellifree <forge.delfadortitan@gmail.com>
2022-01-04 19:33:31 +0100

    feat(task_3): implementing solution

--- src/task_3/main.py
+++ src/task_3/main.py
@@ -1,5 +1,24 @@
+import numpy
+
+def chess_pattern(n):
+    result = numpy.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            if(i % 2 == 0):
+                if(j % 2 == 0):
+                    result[i][j] = -1
+                else:
+                    result[i][j] = 1
+            else:
+                if(j % 2 == 0):
+                    result[i][j] = 1
+                else:
+                    result[i][j] = -1
+    return result
+
+
 def main():
-    pass
+    print(chess_pattern(8))
 
 if __name__ == '__main__':
     main()
