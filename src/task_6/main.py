1218601edefb574e31ede863922f3a7d64a7c446
Gellifree <forge.delfadortitan@gmail.com>
2022-01-04 18:39:53 +0100

    feat(task_6): implementing recursive solution

--- src/task_6/main.py
+++ src/task_6/main.py
@@ -1,5 +1,31 @@
+# Solve this task with recursive programming.
+# In this exercise you have to find how much cookies can be consumed with the
+# given amount of money.
+
+
+
+def eat(money, price, wrap, cookies, cookies_consumed, wrappers_have):
+    if(money > price):
+        cookies = int(money / price)
+        money -= cookies * price
+        eat(money, price, wrap, cookies, cookies_consumed, wrappers_have)
+    elif(cookies > 0):
+        cookies_consumed[0] += cookies
+        wrappers_have = cookies
+        cookies = int(wrappers_have / wrap)
+        wrappers_have = 0
+        eat(money, price, wrap, cookies, cookies_consumed, wrappers_have)
+
 def main():
-    pass
+    money = 16
+    price = 2
+    wrap = 2
+
+    cookies = 0
+    cookies_consumed = [0]
+    wrappers_have = 0
+    eat(money, price, wrap, cookies, cookies_consumed, wrappers_have)
+    print(f'The number of consumed cookies is {cookies_consumed[0]}.')
 
 if __name__ == '__main__':
     main()
