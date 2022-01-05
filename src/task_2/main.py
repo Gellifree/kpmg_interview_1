26eb2db64e6f36ee474c35fae13f9260515979b9
Gellifree <forge.delfadortitan@gmail.com>
2022-01-05 17:16:33 +0100

    feat(task_2): implementing solution with 'obstacles'
    should check later for optimizaton

--- src/task_2/main.py
+++ src/task_2/main.py
@@ -1,31 +1,134 @@
-def count_number_of_attacks(n, r_q, c_q, obstacles):
-    sum = 0
-    for i in range(n, 0, -1):
-        for j in range(1,n+1):
-            if(i == r_q and j == c_q):
-                print("[Q] ", end="")
-            elif(j == c_q and i > r_q):
-                print("[T] ", end="")
-                sum += 1
-            elif(j == c_q and i < r_q):
-                print("[B] ", end="")
-                sum += 1
-            elif(i == r_q and j > c_q):
-                print("[R] ", end="")
-                sum += 1
-            elif(i == r_q and j < c_q):
-                print("[L] ", end="")
-                sum += 1
-            elif(r_q + c_q == i + j):
-                print("[h] ", end="")
-                sum += 1
-            elif(r_q - c_q == i - j):
-                print("[H] ", end="")
-                sum += 1
+import numpy
+
+
+# Check a given position for obstacle
+def is_there_an_obstacle(i,j,obstacles):
+    for obstacle in obstacles:
+        if(i == obstacle[0]-1 and j == obstacle[1]-1):
+            #print(f"obstacle in: {obstacle[0]} and {obstacle[1]}")
+            return True
+    return False
+
+# Draw the map out in the right order
+def draw_map(map, obstacles):
+    print()
+    for i in range(len(map)-1, -1, -1):
+        for j in range(len(map)):
+            if(is_there_an_obstacle(i,j,obstacles)):
+                print("[X]", end=" ")
+            elif(map[j][i] == 0):
+                print("[ ]", end=" ")
             else:
-                print("[ ] ", end="")
+                print(f"[{map[j][i]}]", end=" ")
         print()
-    return sum
+
+# Generating the possible squares
+def generate_map(n, r_q, c_q, obstacles):
+    sum = 0
+    result = []
+    # Matrix filled with zeroes
+    for i in range(n):
+        result.append([])
+        for j in range(n):
+            result[i].append(0)
+
+
+    ## Generating the different 'sides' of the queen
+
+    # Right side
+    for i in range(c_q, n+1):
+        if(is_there_an_obstacle(r_q-1,i, obstacles) == True):
+            result[i-1][r_q-1] = '1'
+            break;
+        else:
+            result[i-1][r_q-1] = '1'
+
+    # Left side
+    for i in range(c_q, 0, -1):
+        if(is_there_an_obstacle(r_q-1,i - 1, obstacles) == True):
+            break;
+        else:
+            result[i-1][r_q-1] = '1'
+
+    # Top
+    for i in range(r_q, n + 1):
+        if(is_there_an_obstacle(i-1,c_q-1, obstacles) == True):
+            #print('obstacle!')
+            break;
+        else:
+            result[c_q-1][i-1] = '1'
+
+    # Bottom
+    for i in range(r_q, 0, -1):
+        if(is_there_an_obstacle(i-1,c_q-1, obstacles) == True):
+            break;
+        else:
+            result[c_q-1][i-1] = '1'
+
+    # top right diagonal
+    j = r_q
+    for i in range(c_q, n + 1):
+        if(is_there_an_obstacle(j-1,i-1,obstacles) == True):
+            break;
+        else:
+            result[i-1][j-1] = '1'
+        j += 1
+        if(j > n):
+            break
+
+
+    # top left diagonal
+    i = c_q
+    for j in range(r_q, n+1):
+        if(is_there_an_obstacle(j-1,i-1,obstacles) == True):
+            break;
+        else:
+            result[i-1][j-1] = '1'
+        i -= 1
+        if(i <= 0):
+            break
+
+
+
+    # bottom right diagonal
+    i = c_q
+    for j in range(r_q, 0, -1):
+        #print(f"{j},{i}")
+
+        if(is_there_an_obstacle(j-1,i-1,obstacles) == True):
+            break;
+        else:
+            result[i-1][j-1] = '1'
+        i += 1
+        if(i > n):
+            break
+
+
+    # bottom left diagonal
+    j = r_q
+    for i in range(c_q, 0, -1):
+        if(is_there_an_obstacle(j-1,i-1,obstacles) == True):
+            break;
+        else:
+            result[i-1][j-1] = '1'
+
+        j -= 1
+        if(j <= 0):
+            break
+
+    # 'Put' the Queen in the right position
+    result[c_q-1][r_q-1] = 'Q'
+    draw_map(result, obstacles)
+    return result
+
+# Sum the amount of 'ones', which represent the attackable squares
+def count_attackable_squares(map):
+    result = 0
+    for i in range(len(map)):
+        for j in range(len(map)):
+            if(map[i][j] == '1'):
+                result += 1
+    return result
 
 def main():
     n = 8
@@ -34,8 +137,9 @@ def main():
     obstacles = [
         [3,5]
     ]
-    sum = count_number_of_attacks(n, r_q, c_q, obstacles)
-    print(f"\nSum of the squares: {sum}")
+    map = generate_map(n, r_q, c_q, obstacles)
+    result = count_attackable_squares(map)
+    print(f"\nThe Queen can attack {result} squares.")
 
 if __name__ == '__main__':
     main()
